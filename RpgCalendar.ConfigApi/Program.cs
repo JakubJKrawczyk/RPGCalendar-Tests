bf9c659ac5e5d6db1755fd68610ca947ce3902aa
JakubKrawczyk <36759354+JakubJKrawczyk@users.noreply.github.com>
2024-08-31 15:14:37 +0200

    dodano brakujące pliki

---
+++ RpgCalendar.ConfigApi/Program.cs
@@ -0,0 +1,26 @@
+using RpgCalendar.ConfigApi;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.MapGet("/config", () => ExternalFunctions.GetEnvs())
+    .WithName("GetEnvs")
+    .WithOpenApi();
+
+app.Run();
+
+
+
