89e06ab843eb53a8090904131389dd7e688c260d
Grzegorz Kramarz <gkramarz222@interia.pl>
2024-10-02 11:12:34 +0200

    Zrobiłem kilka modeli w prywatnym kalendarzu ale do weryfikacji.

--- RpgCalendar.ApiClients/InternalApi/Models/group.cs
+++ RpgCalendar.ApiClients/InternalApi/Models/group.cs
@@ -1,10 +1,12 @@
 ﻿
+using System.Buffers.Text;
+
 namespace RpgCalendar.ApiClients.InternalApi.Models;
 
 public class group
 {
     public Guid Id { get; set; }
     public string Name { get; set; }
-    public string Picture { get; set; }
+    public string profilePicture { get; set; }
     public DateTime CreatedAt { get; set; }
 }
--- RpgCalendar.ApiClients/InternalApi/Models/privateEvent.cs
+++ RpgCalendar.ApiClients/InternalApi/Models/privateEvent.cs
@@ -1,5 +1,45 @@
-﻿namespace RpgCalendar.ApiClients.InternalApi.Models;
+﻿using System.Runtime.InteropServices.JavaScript;
+using NUnit.Framework.Constraints;
 
-public class privateEvent {
+namespace RpgCalendar.ApiClients.InternalApi.Models;
+
+public class usersAbsences {
+    public TimeSpan startingHour { get; set; }
+    public DateOnly startingDay { get; set; }
+    public DateOnly endingDay { get; set; }
+    public TimeSpan endingHour { get; set; }
+}
+public class listEvent {
     public string title { get; set; }
+    public string description { get; set; }
+    public TimeSpan startingHour { get; set; }
+    public DateOnly startingDay { get; set; }
+    public DateOnly endingDay { get; set; }
+    public TimeSpan endingHour { get; set; }
+}
+public class addEvent {
+    public Guid privateEventId { get; set; }
+    public string title { get; set; }
+    public string description { get; set; }
+    public DateOnly startingDay { get; set; }
+    public DateOnly endingDay { get; set; }
+    public Boolean isOnLine { get; set; } // Nie wiem czy dobrze
+    public string eventLocation { get; set; }
+}
+public class deleteEvent {
+    public Guid privateEventId { get; set; }
+    public string title { get; set; }
+    public string description { get; set; }
+    public DateOnly startingDay { get; set; }
+    public DateOnly endingDay { get; set; }
+    public Boolean isOnLine { get; set; } // Nie wiem czy dobrze
+    public string eventLocation { get; set; }
+}
+public class modifyEvent {
+    public string title { get; set; }
+    public string description { get; set; }
+    public DateOnly startingDay { get; set; }
+    public DateOnly endingDay { get; set; }
+    public Boolean isOnLine { get; set; } // Nie wiem czy dobrze
+    public string eventLocation { get; set; }
 }
